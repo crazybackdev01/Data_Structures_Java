355948833e3baea5a80cfff86c793fa65030e21a
Udit Nagar <uditnagar419@gmail.com>
2024-08-09 22:41:23 +0530

    Strings

--- Strings/Basics.java
+++ Strings/Basics.java
@@ -1,5 +1,15 @@
 package Strings;
 
-public class Basics {
+import java.util.Scanner;
 
+public class Basics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st1 = sc.nextLine();
+        String st2 = sc.next();
+        System.out.println("Next Line: " + st1);
+        System.out.println("Next() : " + st2);
+
+        sc.close();
+    }
 }
