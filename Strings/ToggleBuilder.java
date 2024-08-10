e8e2ffd424650382ca34de4d4ee1da683ed846ab
Udit Nagar <uditnagar419@gmail.com>
2024-08-10 18:45:19 +0530

    Added Toggle function

---
+++ Strings/ToggleBuilder.java
@@ -0,0 +1,33 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class ToggleBuilder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder str = new StringBuilder(sc.nextLine());
+
+        for (int i = 0; i < str.length(); i++) {
+            boolean flag = true; // Let all character in the string be Capital
+            char ch = str.charAt(i);
+            if (ch == ' ')
+                continue;
+            int ascii = (int) ch;
+
+            if (ascii >= 97)
+                flag = false;
+
+            if (flag) {
+                ascii += 32;
+                char r_ch = (char) ascii;
+                str.setCharAt(i, r_ch);
+            } else {
+                ascii -= 32;
+                char r_ch = (char) ascii;
+                str.setCharAt(i, r_ch);
+            }
+        }
+
+        System.out.println(str);
+    }
+}
